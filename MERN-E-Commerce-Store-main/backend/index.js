dc32013f0315628f902b0344eafccd2d08baae19
swamisamarth526-design <swamisamarth526@gmail.com>
2026-04-13 12:04:26 +0530

    Add Data

--- MERN-E-Commerce-Store-main/backend/index.js
+++ MERN-E-Commerce-Store-main/backend/index.js
@@ -35,5 +35,9 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
